612f4492a83d017badd28ad2834c5becffaa5f2f
dataset81 <dataset81@gmail.com>
2019-01-06 06:51:06 +0100

    While Loops

--- Loops.R
+++ Loops.R
@@ -11,3 +11,13 @@ while(counter < 12) {
   print(counter)
   counter <- counter + 1
 }
+
+#For loop
+for(i in 1:5){
+  print("Hello R")
+}
+
+i <- 5
+for(i in 5:10){
+  print("I am iterating here, the number", z<- 5 + i)
+}
