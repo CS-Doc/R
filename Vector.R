b3a27edbd3da3f80d21a6bacea71e74b2ca89974
dataset81 <dataset81@gmail.com>
2019-01-08 04:57:38 +0100

    Accessing Vectors

---
+++ Vector.R
@@ -0,0 +1,26 @@
+Vector <- c(3, 45, 56, 732)
+Vector
+is.numeric(Vector)
+is.integer(Vector)
+is.double(Vector)
+
+V2 <- c(2L, 12L, 243L, 0L)
+is.numeric(V2)
+is.integer(V2)
+
+V3 <- c("a", "B23", "Hello", 7)
+V3
+is.character(V3)
+seq(1,15)
+1:15
+seq(1,15,2)
+z <- seq(1,15,4) 
+z
+rep(3, 50)
+d <- rep(3, 50)
+d
+
+rep("a", 50)
+x <- c(80, 20)
+y <- rep(x,10)
+y
