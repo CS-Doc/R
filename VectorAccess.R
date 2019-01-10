b6a633c4f5506ad11e87c78dd5b8962d41c1e633
dataset81 <dataset81@gmail.com>
2019-01-10 03:59:28 +0100

    Nitty Gritty of Vectors

--- VectorAccess.R
+++ VectorAccess.R
@@ -13,3 +13,48 @@ w[-3:-5]
 #single number is Vector
 #Vectors canbu used in fuctions and functions can 
 #produce array
+
+x <- rnorm(5)
+x
+#R Specific programming loop, iterating through the vector
+for (i in x){
+  print(i)
+}
+#loops are for iterative process "Monkey Jobs"
+print(x[1])
+print(x[2])
+print(x[3])
+print(x[4])
+print(x[5])
+
+#conventional programming loop, iterating through 
+#the position
+for(j in 1:5){
+print(x[j])
+}
+
+#--------------------------------------------------------
+
+N <- 100
+a <- rnorm(N)
+b <- rnorm(N)
+
+#Vectorized approach (faster, less code, no loop):
+
+c <- a * b
+
+
+#De-Vectorized approach:
+
+#Empty vector we are allocating memory with 100 spaces
+d <- rep(NA,N) 
+for (i in 1:N){
+  d[i] <- a[i] * b[i]
+}
+
+#Why in Vectorized approach in R is faster? 
+#Since vectors are only of same type in R (int/dou/str)
+#It is  delegating to C or Fortran that fact
+#which then makes the rest of the delegated lang faster
+
+
