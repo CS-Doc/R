e61e17ba6dbdfb85525c2f5f582cc2f3af8faaa0
dataset81 <dataset81@gmail.com>
2019-01-10 03:35:47 +0100

    Power of R with Vectors

--- VectorAccess.R
+++ VectorAccess.R
@@ -4,3 +4,12 @@ w[c(1, 3, 5)]
 w[-1]
 w[1:3]
 w[-3:-5]
+#vector arithmetics, r is vectorized programming language
+#no need for loops just add vector a to vector b
+#you can also perfrom boolean operation between vectors
+#Recycling of Vector- When one vector is smaller, R will 
+#recycle it to use again with the other bigger one
+#vector is the basic building block of R, even a
+#single number is Vector
+#Vectors canbu used in fuctions and functions can 
+#produce array
